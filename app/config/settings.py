ac6db8c87fa438dd07e11c974ed535b6bd3a8e3a
guilherme096 <guilherme.rosa60@gmail.com>
2025-04-02 11:47:40 +0100

    change env name to match azure standards

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLEMAPAPIKEY")
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLEMAPSAPIKEY")
 
 if not GOOGLE_MAPS_API_KEY:
     raise ValueError("GOOGLE_MAPS_API_KEY is missing or not loaded correctly")
-
